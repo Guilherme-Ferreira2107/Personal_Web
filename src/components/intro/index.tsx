e98ee4377c130c9a306b1418860f6de4adfd52db
Guilherme Ferreira <guilhermeferreira2107@gmail.com>
2020-07-02 17:57:06 -0300

    Fix position in Name and Title

--- src/components/intro/index.tsx
+++ src/components/intro/index.tsx
@@ -1,30 +1,14 @@
 import React from 'react';
 import './styles.scss';
+import Name from './name';
 
 const Intro = () => {
   return (
     <div id="intro">
       <div className="container container-fluid">
-        <div className="Nome text-center">
-        <ul>
-          <li>G</li>
-          <li>U</li>
-          <li>I</li>
-          <li>L</li>
-          <li>H</li>
-          <li>E</li>
-          <li>R</li>
-          <li>M</li>
-          <li>E </li>
-          <li>S</li>
-          <li>A</li>
-          <li>N</li>
-          <li>T</li>
-          <li>O</li>
-          <li>S</li>
-          <li>_</li>
-        </ul>
-        <h4>Development FullStack in React and NodeJs</h4><br />
+        <div className="box-name text-center">
+          <Name />
+          <h4>Development Full Stack in React.js and Node.js</h4><br />
         </div>
       </div>
     </div>
