e14b93fdacf21dd2a300e5ebe6b63bd2a8e9a073
Guilherme Ferreira <guilhermeferreira2107@gmail.com>
2020-07-03 18:17:26 -0300

    Test with Motion in Intro

--- src/components/intro/index.tsx
+++ src/components/intro/index.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import './styles.scss';
 
-const Intro = () => {
-  return (
-    <div id="intro">
-      <div className="container container-fluid">
-        <div className="box-name text-center">
-          <h2>GUILHERME SANTOS<span>_</span></h2>
-          <h4>Development Full Stack in React.js and Node.js</h4><br />
-        </div>
-      </div>
-    </div>
-  );
+import {Motion, spring} from 'react-motion';
+import { red } from 'color-name';
+
+  class Intro extends React.Component {
+    state = {
+      left: 0
+    };
+  
+    render () {
+      return (
+        <React.Fragment>
+          <div id="intro">
+            <div className="container container-fluid">
+              <div className="box-name text-center">
+                <Motion defaultStyle={{ left: -500 }} style={{ left: spring(10) }}>
+                  {val => <h2 style={{ position: 'relative', ...val }}>
+                  <h2>GUILHERME SANTOS<span>_</span></h2>
+                  </h2>}
+                  </Motion>
+                  <Motion defaultStyle={{ left: -800 }} style={{ left: spring(10) }}>
+                  {val => <h2 style={{ position: 'relative', ...val }}>
+                  <h4>Development Full Stack in React.js and Node.js</h4><br />
+                  </h2>}
+                </Motion>
+              </div>
+            </div>
+          </div>
+        </React.Fragment>
+      );
+    }
 }
 
 export default Intro;
